72235816d3ac6d155da0e44f8fa105369c68e247
itstoomanymichaels <maikeruh03@gmail.com>
2022-08-25 10:02:44 -0400

    Implemented playerSelection function and changed to return

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
 function getComputerChoice() {
     let choice = Math.ceil(Math.random() * 3);
     if (choice === 1) {
-        console.log("Earth!");
+        return "earth";
     } else if (choice === 2) {
-        console.log("Water!");
+        return "water";
     } else {
-        console.log("Fire!");
+        return "fire";
     }
 }
 
+function playerSelection() {
+    let choice = prompt("Which element do you choose?").toLowerCase();
+    if (choice !== "earth" && choice !== "water" && choice !== "fire") {
+        console.log("Not a valid choice! Stop cheating!");
+    } else {
+        return choice;
+    }
+}
+
+
 getComputerChoice();
+playerSelection();
