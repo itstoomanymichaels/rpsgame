93f728c3434cd1ceaf11b3eb611ec95958ef66c8
itstoomanymichaels <maikeruh03@gmail.com>
2022-08-25 10:12:28 -0400

    Implemented rps  game using if and switch conditionals

--- javascript.js
+++ javascript.js
@@ -18,6 +18,44 @@ function playerSelection() {
     }
 }
 
-
-getComputerChoice();
-playerSelection();
+function ewf(computerChoice, playerChoice) {
+    const computer = getComputerChoice();
+    const player = playerSelection();
+    if (computer === "fire") {
+        switch (player) {
+            case "earth":
+                return "You lose! Fire beats earth.";
+                break;
+            case "fire":
+                return "It's a draw!";
+                break;
+            case "water":
+                return "You win! Water beats fire.";
+                break;
+        }
+    } else if (computer === "earth") {
+        switch (player) {
+            case "earth":
+                return "It's a draw!";
+                break;
+            case "fire":
+                return "You win! Fire beats earth.";
+                break;
+            case "water":
+                return "You lose! Earth beats water.";
+                break;
+        }
+    } else {
+        switch (player) {
+            case "earth":
+                return "You win! Earth beats water.";
+                break;
+            case "fire":
+                return "You lose! Water beats fire.";
+                break;
+            case "water":
+                return "It's a draw!";
+                break;
+        }
+    }
+}
