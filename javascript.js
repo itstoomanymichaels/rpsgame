247a18ad1b7d5147c46630c44c5dc208c66a9322
itstoomanymichaels <maikeruh03@gmail.com>
2022-08-25 10:23:47 -0400

    Checked to make sure everything was working, game function seems to be the problem

--- javascript.js
+++ javascript.js
@@ -18,44 +18,69 @@ function playerSelection() {
     }
 }
 
-function ewf(computerChoice, playerChoice) {
+function playRound() {
     const computer = getComputerChoice();
     const player = playerSelection();
     if (computer === "fire") {
         switch (player) {
             case "earth":
-                return "You lose! Fire beats earth.";
+                console.log("You lose! Fire beats earth.");
                 break;
             case "fire":
-                return "It's a draw!";
+                console.log("It's a draw!");
                 break;
             case "water":
-                return "You win! Water beats fire.";
+                console.log("You win! Water beats fire.");
                 break;
         }
     } else if (computer === "earth") {
         switch (player) {
             case "earth":
-                return "It's a draw!";
+                console.log("It's a draw!");
                 break;
             case "fire":
-                return "You win! Fire beats earth.";
+                console.log("You win! Fire beats earth.");
                 break;
             case "water":
-                return "You lose! Earth beats water.";
+                console.log("You lose! Earth beats water.");
                 break;
         }
     } else {
         switch (player) {
             case "earth":
-                return "You win! Earth beats water.";
+                console.log("You win! Earth beats water.");
                 break;
             case "fire":
-                return "You lose! Water beats fire.";
+                console.log("You lose! Water beats fire.");
                 break;
             case "water":
-                return "It's a draw!";
+                console.log("It's a draw!");
                 break;
         }
     }
 }
+
+playRound();
+/*
+function game() {
+    let playerw = 0;
+    let computerw = 0;
+    for (let i = 0; i < 5; i++) {
+        let winner = playRound();
+        if (winner.contains("win")) {
+            playerw += 1;
+        } else if (winner.contains("lose")) {
+            computerw += 1;
+        }
+    }
+    if (playerw > computerw) {
+        console.log("Congrats! You won the game!");
+    } else if (playerw < compueterw) {
+        console.log("Shucks, you lost! That's too bad.");
+    } else {
+        console.log("It's a draw! Great choices from both sides!");
+    }
+}
+
+game();
+*/
